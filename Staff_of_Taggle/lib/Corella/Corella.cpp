1e19db88f87eda41e572cbc9553054660110de56
NigelB <nigel.blair@gmail.com>
2019-09-26 09:46:02 +1000

    Add license to files.

--- Staff_of_Taggle/lib/Corella/Corella.cpp
+++ Staff_of_Taggle/lib/Corella/Corella.cpp
@@ -1,6 +1,21 @@
-//
-// Created by nigelb on 2/09/2019.
-//
+/*
+    Staff_Of_Taggle sends GPS positions via a Taggle Corella to be used to
+    calibrate the Taggle Multilateration.
+    Copyright (C) 2019  Nigel Bajema eResearch Center James Cook University
+
+    This program is free software: you can redistribute it and/or modify
+            it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+            but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 #include "Corella.h"
 
